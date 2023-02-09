6ae65317831d8f96e5044adadacc6172a7fe2992
richardmundyiii <mundy.richard@icloud.com>
2023-02-09 15:08:57 -0800

    inital commit

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  googleId: {
+    type: String,
+    required: true,
+  },
+  email: String,
+  avatar: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
