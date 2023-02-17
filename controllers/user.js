420311c9dc9afe092d93d7759748fafcd18b53e7
richardmundyiii <mundy.richard@icloud.com>
2023-02-16 19:18:12 -0800

    fix profile page

--- controllers/user.js
+++ controllers/user.js
@@ -6,26 +6,22 @@ module.exports = {
 };
 
 function index(req, res) {
-  Listing.find({}, function (err, listings) {
-    const myListings = Listing.find(
-      { host: req.user._id },
-      function (err, listing) {}
-    );
-    console.log(myListings);
-    let reservationR = [];
-    listings.forEach((el) => {
-      el.reservations.forEach((r) => {
-        console.log(r.host);
-        console.log(req.user._id);
-        if (r.host.equals(req.user._id)) {
-          reservationR.push(el);
-        }
+  Listing.find({})
+    .populate("reservations")
+    .exec(function (err, listings) {
+      const myListings = listings.filter((el) => el.host.equals(req.user._id));
+      let reservationR = [];
+      listings.forEach((el) => {
+        el.reservations.forEach((r) => {
+          if (r.host.equals(req.user._id)) {
+            reservationR.push(el);
+          }
+        });
+      });
+      res.render("user/index", {
+        title: "User Profile",
+        reservationR,
+        myListings,
       });
     });
-    res.render("user/index", {
-      title: "User Profile",
-      listings,
-      reservationR,
-    });
-  });
 }
